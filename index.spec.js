7ec06fa484c3aa00ec4b2973fc55cc39a79c7349
Touffaya <abourjac@octo.com>
2016-09-19 14:28:28 +0200

    chores: space test file

--- index.spec.js
+++ index.spec.js
@@ -3,7 +3,9 @@ const expect = chai.expect;
 const server = require('./index.js');
 
 describe('Server', () => {
+
   describe('/', () => {
+
     it('should return hello world!', (done) => {
       server.inject('/', (res) => {
         expect(res.payload).to.equal('Hello world!');
@@ -11,7 +13,9 @@ describe('Server', () => {
       });
     });
   });
+
   describe('/pokemons', () => {
+
     it('should return a list of pokemons', (done) => {
       server.inject('/pokemons', (res) => {
         expect(res.result).to.deep.equal([
@@ -23,22 +27,30 @@ describe('Server', () => {
       });
     });
   });
+
+
   describe('POST', () => {
+
     describe('When payload is undefined', () => {
+
       it('should send a 400 status code', (done) => {
         server.inject({url: '/pokemons', method: 'POST'}, (res) => {
           expect(res.statusCode).to.equal(400);
           done();
         });
       });
+
     });
+
     describe('When payload is undefined', () => {
+
       it('should send a 201 status code', (done) => {
         server.inject({url: '/pokemons', method: 'POST', payload: {name: 'evoli'}}, (res) => {
           expect(res.statusCode).to.equal(201);
           done();
         });
       });
+
       it('should create the pokemon given in the payload', (done) => {
         server.inject({url: '/pokemons', method: 'POST', payload: {name: 'bulbizarre'}}, (res) => {
           server.inject({url: '/pokemons'}, (response) => {
