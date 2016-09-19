62cbb0e655ff70faf759820ec5fcfb4a88613199
Touffaya <abourjac@octo.com>
2016-09-19 16:19:30 +0200

    chores: refactor with conf file

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+/* ------- MOD -------*/
 const hapi = require('hapi');
 const swagger = require('hapi-swagger');
 const vision = require('vision');
 const inert = require('inert');
 
+/* ------- FS ------- */
+const config = require('./config');
 const routes = require('./routes.js');
 
-var server = new hapi.Server();
-server.connection({port:3000});
+/* ------- RES ------- */
 
+/* ------- SRC ------- */
+const server = new hapi.Server();
+
+server.connection({port: config.port});
 server.route(routes);
-
 server.register([inert, vision, swagger], (err) => {
   if (err) {
     console.log(err);
@@ -18,7 +23,7 @@ server.register([inert, vision, swagger], (err) => {
 
 server.start(function (err) {
   if (err) throw err;
-  console.log('server listening on port 3000');
+  console.log('server listening on port ' + config.port );
 });
 
 module.exports = server;
